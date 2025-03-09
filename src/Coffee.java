44a6152e6f611dc687f5343fe4b00987acf9a23b
Veronika <veronika853@yandex.ru>
2025-03-09 13:52:30 +0300

    Реализован класс Coffee

--- src/Coffee.java
+++ src/Coffee.java
@@ -1,35 +1,59 @@
 /**
  * Класс Coffee
- *
+ * <p>
  * Поля (примерная идея):
- *   - private String name;       // Название кофе
- *   - private double basePrice;  // Базовая цена
- *
+ * - private String name;       // Название кофе
+ * - private double basePrice;  // Базовая цена
+ * <p>
  * Геттеры/сеттеры:
- *   - getName(), setName(...)
- *   - getBasePrice(), setBasePrice(...)
- *
+ * - getName(), setName(...)
+ * - getBasePrice(), setBasePrice(...)
+ * <p>
  * Методы (примерные идеи):
- *   - calculatePriceWithSugar(Sugar sugar): double
- *       (может возвращать basePrice + некая надбавка за сахар, или просто basePrice, если надбавки нет)
- *
- *   - toString() (не обязательно, но может упростить вывод информации)
+ * - calculatePriceWithSugar(Sugar sugar): double
+ * (может возвращать basePrice + некая надбавка за сахар, или просто basePrice, если надбавки нет)
+ * <p>
+ * - toString() (не обязательно, но может упростить вывод информации)
  */
 public class Coffee {
+
+
     // Напишите код здесь (объявление полей)
+    private String name;
+    private double basePrice;
+
 
     // Напишите код здесь (конструктор/конструкторы)
+    public Coffee(double basePrice, String name) {
+        this.name = name;
+        this.basePrice = basePrice;
+
+    }
 
     // Напишите код здесь (геттеры/сеттеры)
+    public String getName() {
+        return name;
+    }
 
-    // public double calculatePriceWithSugar(Sugar sugar) {
-    //     // Напишите код здесь:
-    //     // Пример:
-    //     // double finalPrice = basePrice + sugar.getSugarPrice();
-    //     // return finalPrice;
-    // }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-    // public String toString() {
-    //     // Напишите код здесь (опционально)
-    // }
+    public double getBasePrice() {
+        return basePrice;
+    }
+
+    public void setBasePrice(double basePrice) {
+        this.basePrice = basePrice;
+    }
+
+    public double calculatePriceWithSugar(Sugar sugar) {
+        //     // Напишите код здесь:
+        return basePrice + sugar.getSugarPrice();
+    }
+
+    public String toString() {
+        //     // Напишите код здесь (опционально)
+        return "Coffee{name='" + name + "', basePrice=" + basePrice + " руб.}";
+    }
 }
